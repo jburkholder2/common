67e5bc0f78763f95854bf6cd9786fcb0b330bb38
Jake <jake@localhost.localdomain>
2019-03-20 20:44:35 -0700

    Commiting sources.

---
+++ cppflags.h
@@ -0,0 +1,7 @@
+#define _STDINT_H
+#define	_BITS_TYPES_H
+#define	__FD_SETSIZE	1024
+#define	LIBGCC_S_SO		"libgcc_s.so.1"
+#define	WEOF	0xffffu
+#define	INT64_C(s)	s ## LL
+#define	UINT64_C(s)	s ## ULL
